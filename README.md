d77bf30053a89efead2be2bb5ce63a4b3b232b65
Brad Doremus <rbdoremus13@gmail.com>
2020-08-13 16:36:52 -0500

    Push after study group

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# ToDo-List
-Homework #5 - Create Day Planner Project
+# 05 Third-Party APIs: Work Day Scheduler
+
+Brad Doremus
+GitHub: https://github.com/Doremus256/ToDo-List
+Live Site: https://doremus256.github.io/ToDo-List/
+
+For this assignment we were tasked with creating a Day Planner. 
+
+The HTML is pretty straight-forward, with new Divs representing each hour in the day, all with the same class and with corresponding id's so that my JS can hook into individual hours' textarea and apply relevant effects. 
+
+For the JS, my for-loop ran through each div with id based on the hour, and compared to Moment.JS to determine whether each text area was before or after (greater than or less than) the current time. The current time is in military time, so I had to change the ID's in the HTML to match (1pm = 13, 2pm = 14, etc). Also, since Moment.JS pulls military time in a weird format, I used parseInt to change the time to a single intiger, which can then be compared to each hour's textarea's ID (8 for 8:00am, all the way t0 17 for 5:00pm)
+
+Finally, to save to local storage I used localStorage.setItem to its inputted value. 
+
+Similar to the last homework, I am looking forward to coming back to this assignment later when I am more polished, and creating a better application. 
+
+This is the best I could do for now. 
+
+Thank You!
---
+++ script.js
@@ -0,0 +1,67 @@
+// Variables here
+const date = $("#currentDay")
+let textArea = $("textarea");
+const now = moment().format("MMM Do YYYY")
+const clock = parseInt(moment().format('H'));
+
+
+// Today's date
+date.text(now);
+
+// For box colors, based on current time
+
+for(let i=0; i<textArea.length; i++) {
+    let timeColor = parseInt(textArea[i].id)
+    if (timeColor < clock) {
+        textArea[i].setAttribute("class", "past");
+    }
+    else if (timeColor > clock) {
+        textArea[i].setAttribute("class", "future");
+    }
+    else {
+        textArea[i].setAttribute("class", "present");
+    }
+}
+
+// Buttons that send input info to local storage
+
+$("8amBtn").on("click", function() {
+    let eightTask = $("#8").val();
+    localStorage.setItem("8am", eightTask);
+})
+$("9amBtn").on("click", function() {
+    let nineTask = $("#9").val();
+    localStorage.setItem("9am", nineTask);
+})
+$("10amBtn").on("click", function() {
+    let tenTask = $("#10").val();
+    localStorage.setItem("10am", tenTask);
+})
+$("11amBtn").on("click", function() {
+    let elevenTask = $("#11").val();
+    localStorage.setItem("11am", elevenTask);
+})
+$("12pmBtn").on("click", function() {
+    let twelveTask = $("#12").val();
+    localStorage.setItem("12pm", twelveTask);
+})
+$("1pmBtn").on("click", function() {
+    let oneTask = $("#1").val();
+    localStorage.setItem("1pm", oneTask);
+})
+$("2pmBtn").on("click", function() {
+    let twoTask = $("#2").val();
+    localStorage.setItem("2pm", twoTask);
+})
+$("3pmBtn").on("click", function() {
+    let threeTask = $("#3").val();
+    localStorage.setItem("3pm", threeTask);
+})
+$("4pmBtn").on("click", function() {
+    let fourTask = $("#4").val();
+    localStorage.setItem("4pm", fourTask);
+})
+$("5pmBtn").on("click", function() {
+    let fiveTask = $("#5").val();
+    localStorage.setItem("5pm", fiveTask);
+})
